d78bdbfde7df815a4f50788178d49fa599bc2343
wo1no <97290257+wo1no@users.noreply.github.com>
2022-01-22 06:28:19 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,22 +7,22 @@ fi
 echo ${VER}
 
 if [[ -z "${Vless_Path}" ]]; then
-  Vless_Path="/s233"
+  Vless_Path="/SvMm233"
 fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vless_UUID="f36925e8-d0f3-4953-b8fa-f25d9721ed2b"
 fi
 echo ${Vless_UUID}
 
 if [[ -z "${Vmess_Path}" ]]; then
-  Vmess_Path="/s244"
+  Vmess_Path="/SvMm244"
 fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="f36925e8-d0f3-4953-b8fa-f25d9721ed2b"
 fi
 echo ${Vmess_UUID}
 
